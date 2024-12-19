e0efe8382ca817eaba6bc3ebb6be95ccbab3b886
Максим <141343669+Gustav6dbjd@users.noreply.github.com>
2024-12-19 17:32:11 +0300

    new tasks

---
+++ script.js
@@ -0,0 +1,24 @@
+
+ let buttons = document.querySelectorAll('button');
+ buttons.forEach(button => {
+    button.textContent = 'Нажми меня';
+ });
+
+
+
+ let container = document.querySelector('.container');
+ if (container) {
+    container.classList.add('active');
+ }
+
+
+
+let parentElement = document.getElementById('parent');
+if (parentElement) {
+    let descendants = parentElement.getElementsByTagName('*');
+    console.log('Количество потомков:', descendants.length);
+} else {
+    console.log('Элемент с идентификатором "parent" не найден.');
+}
+
+
